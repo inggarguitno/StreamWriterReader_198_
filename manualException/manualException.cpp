5247f52085888f642c816582765f16b96bbdff86
inggarguitno <inggar.kinanthi.ft22@mail.umy.ac.id>
2023-06-19 03:58:21 +0700

    try

--- manualException/manualException.cpp
+++ manualException/manualException.cpp
@@ -6,7 +6,7 @@ int main()
 	try {
 		cout << "Selamat Belajar di Prodi TI UMY" << endl;
 		throw 0.5; //melemparkan sebuah integer maka
-		cout << "Pernyataan tidak akan dieksekusi" << endl;
+		cout << "Pernyataan tidak akan dieksekusi" << endl; 
 	}
 	catch (int a)
 	{

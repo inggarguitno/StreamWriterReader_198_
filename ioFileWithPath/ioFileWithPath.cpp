e71d954607a637cb2f9d0f1b72eb9770dff8f457
inggarguitno <inggar.kinanthi.ft22@mail.umy.ac.id>
2023-06-19 03:56:46 +0700

    while

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -20,7 +20,7 @@ int main() {
 	//unlimited loop untuk menulis
 	while (true) {
 		cout << "_";
-		//mendapatkan setiap karakter dalam satu baris
+		//mendapatkan setiap karakter dalam satu baris 
 		getline(cin, baris);
 		//loop akan berhenti jika anda memasukan karakter q
 		if (baris == "q") break;
